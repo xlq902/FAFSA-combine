285b7c03fe9129e0290990ee917663fab5bdbe24
Eric Yang <tetrgda@gmail.com>
2022-10-05 23:10:44 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Created by Eric Yang for the University of Connecticut, 2022
+
+using System.IO;
+
+string working = "C:\\Users\\tetrg\\OneDrive\\Documents\\Personal\\Marine Research\\Rhoptry Proteins\\ROP18";
+Directory.SetCurrentDirectory(working);
+string[] dirs = Directory.GetDirectories(working);
+
+if (!File.Exists(working + "\\combex\\" + "compfa-ROP18.fasta"))
+{
+    var i = File.Create(working + "\\combex\\" + "compfa-ROP18.fasta");
+    i.Close();
+}
+
+foreach (string dir in dirs)
+{
+    try
+    {
+        Directory.SetCurrentDirectory(dir);
+        string[] dirs2 = Directory.GetFiles(dir);
+
+        foreach (string dir2 in dirs2)
+        {
+            if (dir2.EndsWith(".fasta") || dir2.EndsWith(".fa"))
+            {
+                bool a = dir2.Equals(working + "\\combex\\" + "compfa-ROP18.fasta");
+                if (!a)
+                {
+                    string h = File.ReadAllText(dir2);
+                    string cleaned = h.TrimEnd('\r', '\n');
+                    File.AppendAllText(working + "\\combex\\" + "compfa-ROP18.fasta", cleaned + Environment.NewLine);
+                }
+                Console.WriteLine("Completed directory " + dir);
+            }
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Unknown error occurred in directory " + dir);
+    }
+}
+
+Console.WriteLine("All directories completed.");
---
+++ README.md
@@ -0,0 +1,3 @@
+This is a special script written in c# for neat organization of FASTA files into one FASTA file to make multiple sequence alignments easier.
+
+Written by Eric Yang, 2022
